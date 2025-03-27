8e4c38c0b15d2dd7adef19a20c6f4de12135563e
VIPINRAJK26 <vipinrajk026@gmail.com>
2025-03-27 20:58:29 +0530

    updated layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -49,7 +49,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+    >
       <html lang="en">
         <body className={`${poppins.variable} antialiased`}>
           <main>
